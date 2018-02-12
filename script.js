1d410f88755b7dfdae0018300c1b3c51bb4bff6c
yiiweb <yii.web@yandex.ru>
2018-02-12 23:19:59 +0100

    Style and code clean

--- script.js
+++ script.js
@@ -3,47 +3,29 @@ const text = document.getElementById('text')
 const recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 
-let recognitionStarted = false;
-const startRecordBtn = document.getElementById('startRecord');
-const stopRecordBtn = document.getElementById('stopRecord');
-const instructions = document.querySelector('.instructions');
+const instructions = document.getElementById('instructions');
 
-recognition.onstart = function() {
-  instructions.className = 'instructions';
+const startRecord = () => {
+  instructions.className = '';
   instructions.textContent = 'Запись голоса. Говорите в микрофон';
   instructions.classList.add('success');
 }
 
-recognition.onspeechend = function() {
-  instructions.className = 'instructions';
-  instructions.textContent = 'Запись голоса преостановлена';
+const stopRecord = () => {
+  instructions.className = '';
+  instructions.textContent = 'Запись приостановлена';
   instructions.classList.add('danger');
 }
 
-recognition.onerror = function(event) {
-  if(event.error == 'no-speech') {
-    instructions.className = 'instructions';
-    instructions.textContent ='Голос не был обнаружен. Попробуйте снова';
-    instructions.classList.add('error');
-  };
+const writeText = event => {
+  text.value += event.results[event.resultIndex][0].transcript
 }
 
-recognition.onresult = function(event) {
-  var current = event.resultIndex;
+recognition.onstart = startRecord
 
-  var transcript = event.results[current][0].transcript;
+recognition.onerror = stopRecord
 
-  text.value += transcript;
-}
-
-// startRecord.addEventListener('click', ev => {
-//   recognition.start();
-// })
-
-// stopRecordBtn.addEventListener('click', ev => {
-//   recognition.abort();
-// })
-
+recognition.onresult = writeText
 
 document.addEventListener('keyup', ev => {
   if (ev.key === 'r') {
